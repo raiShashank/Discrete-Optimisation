d25033095333db54c27df6e438a1f03c0fac74db
Shashank Rai <shashankrai02@gmail.com>
2013-08-10 20:22:47 +0530

    Another better implementation using constraint programming

--- nQueens.py
+++ nQueens.py
@@ -3,202 +3,135 @@
 import pylab
 class Chess(object):
 	def __init__(self,dimension):
+		"""
+			row : a list of lists , each list stores values possible for that column.It is basically a decision variable fr my model.If d list contains
+					just one value, gud lse make a choice ,while making choice, I'll send copy of d row frm ith list ,i.e row[i:] since I won't
+					get any benefit by changing values of possible values fr earlier lists since there I have already made the choice
+		"""
 		self.dimension = dimension
 		self.solution = 0
 		self.recursion = 0
 		self.solutionSet = []
-		#self.board = [[0] * dimension for i in xrange(dimension)]
-		self.board = []
+		self.board = [[0] * dimension for i in xrange(dimension)]
+		self.row = []
 		for i in xrange(self.dimension):
-			self.board.append([])
+			self.row.append([])
 			for j in xrange(self.dimension):
-				self.board[i].append(0)
-		
+				self.row[i].append(j)
+			self.row[i].append(self.dimension * self.dimension + self.dimension)	# no. of possible values dese decision variables can take
+			self.row[i].append("False")				# 0 denots dat queen has not been put in dat column
 	def __str__(self):
 		#chessBoard = pylab.array(self.board)
 		#return str(chessBoard)
 		answer = ''
-		for i in self.board:
+		for i in self.row:
 			answer += str(i)
 			answer += '\n'
 		return answer
-	def play(self,chess,row,col):
+	# c.choice(0,c.row[:] , 0)	
+	def choice(self,colNo , row , choice , possibleAnswer):
 		"""
-			chess: configuration of the board rite now
+			choice : row no where we decided 2 plzce d queen in colNo
+			colNo : an int [0, self.dimension) denotes in which column do I have 2 make d choice
+			row : list of lists but colNo  is d first column here i.e row[0] refers to self.row[colNo - 1]	(start column is 0) so that when I backtrack
+					in d fn which called dis invokation, I don't have any effect of dese calls
 		"""
-		self.recursion += 1
-		X = col
-		Y = row
-		#chess[Y][X] = 2
-		self.putConstraint(chess,row,col)
-		#print pylab.array(chess)
-		#print pylab.array(chess)
-		i = 0 
-		j = 0
+		self.prune(colNo , row , choice)
+		row[colNo][-1] = "True"
+		
+		possibleAnswer.append((choice,colNo))
+		#self.board[choice][colNo] = 2
+		#for h in xrange(self.dimension):
+		#	print self.board[h]
+		#print 
+		#for h in xrange(self.dimension):
+						#print row[h]
+		#print "-------------------------"
 		repeat = False
+		#i = 0
+		#while i < 2:
 		while not repeat:
+		
 			repeat = True
-			sigma = 0
-			while i < self.dimension:
-				sigma = 0
-				#print chess[x]
-				#print type(chess[x])
-				#print type(chess[x][0])
-				for j in xrange(self.dimension):	#chess[i] is a complete row
-					sigma += chess[i][j]
-				if sigma == self.dimension:		# all places with 1, ideally v want one queen nd rest all 1.i.e sum = self.dimension + 1
-					print "not possible"
+			for i in xrange(colNo + 1,self.dimension):
+				if row[i][-2] == self.dimension * self.dimension:
+					print "Not Possible"
 					return 1
-				if sigma == self.dimension - 1:
-					
-					for j in xrange(self.dimension):
-						if chess[i][j] == 0:
-							repeat = False
-							#chess[i][j] = 2
-							self.putConstraint(chess,i,j)		#i is row no. j is col no.
-				
-				i += 1
-			j = 0
-			sigma = 0
-			while j < self.dimension:
-				sigma = 0
-				for i in xrange(self.dimension):
-					sigma += chess[i][j]		#for a given col j I want 2 find sun of values in all d rows
-				if sigma == self.dimension:
-					print "not possible"
-					return 1
-				if sigma == self.dimension - 1:
+				if row[i][-2] == self.dimension * self.dimension + 1 and row[i][-1] == "False":
+					rowNo = row[i][0]	#rowNo is a list containg singleton row no. in which d queen can b put, D queen in ith col has 2 b put in dis row
+					self.prune(i , row , rowNo)
+					row[i][-1] = "True"
+					possibleAnswer.append((rowNo,i))
+					#self.board[rowNo][i] = 2
+					#for h in xrange(self.dimension):
+					#	print self.board[h]
+					#print 
+					#for h in xrange(self.dimension):
+						#print row[h]
+					#print
 					repeat = False
-					for i in xrange(self.dimension):
-						if chess[i][j] == 0:
-							#chess[i][j] = 2
-							self.putConstraint(chess,i,j)
-				j += 1
-				
-		# Previous guess for Queen was made in row row, guess fr a suitable place for queen in row + 1
-		count = 1
-		for i in chess:
-			#print i
-			if sum(i) != self.dimension + 1:
-				count = 0
-				break
-		if count == 1:
-			#self.board = chess
-			#print self
-			self.encode(chess)
-			#self.solution += 1
-			return 0
-		i = row + 1
-		while i  < self.dimension:
-			#print i
-			#if j < self.dimension:
-			if sum(chess[i]) == self.dimension + 1:	#d next row already contains a queen
-				i += 1
+					
+			#i += 1
+		#print "Shashank"
+		count = 0
+		for i in xrange(colNo + 1, self.dimension):
+			if row[i][-1] == "True":
+				count += 1
+				continue
 			else:
 				break
-		if i < self.dimension:
-			for j in xrange(self.dimension):
-				if chess[i][j] == 0:
-					#count += 1
-					copy = self.replicate(chess)
-					#print copy == chess
-					#print copy is chess
-					self.play(copy,i,j)		#i denotes d row nd j denotes d column
-					#print "After returning back"
-					#print pylab.array(chess)
-	def encode(self,chess)	:
-		possibleAnswer = []
-		for i in xrange(self.dimension):
-			for j in xrange(self.dimension):
-				if chess[i][j] == 2:
-					possibleAnswer.append((i,j))
-					break							#since it is a possible configuration,hence dere can only b 1 queen in every row
-					
-		if possibleAnswer not in self.solutionSet:
-			self.solutionSet.append(possibleAnswer)
-			self.solution += 1
-	def printSolutions(self):
-		i = 0
-		while i < self.solution:
-			print self.decode(self.solutionSet[i])
-			i += 1
-			
-	def decode(self,coordinates):
-		"""
-			coordinates : list of tuples(row,col) which should be 1
-		"""
-		ans = Chess(len(coordinates))
-		for point in coordinates:
-			ans.board[point[0]][point[1]] = 2
-		return ans
-		
-	def replicate(self, chess)	:
+		if count == self.dimension - colNo - 1:		# all cols have got a queen each
+			print "Got a solution"
+			if possibleAnswer not in self.solutionSet:
+				self.solutionSet.append(possibleAnswer)
+				self.solution += 1
+			return 0
+		if colNo + 1 + count < self.dimension:
+			for rowNo in row[colNo + 1 + count][:-2]:
+				copy = self.replicate(row,0)
+				self.choice(colNo + 1 + count, copy, rowNo , possibleAnswer[:])
+	def prune(self,colNo , row , choice)	:
+		#i = 0
+		for i in xrange(colNo + 1,self.dimension):
+			if choice in row[i]:
+				row[i].remove(choice)
+				row[i][-2] -= 1
+			if choice - (i - colNo) in row[i]:
+				row[i].remove(choice - (i - colNo))
+				row[i][-2] -= 1
+			if choice + (i - colNo) in row[i]:
+				row[i].remove(choice + (i - colNo))
+				row[i][-2] -= 1
+	def replicate(self, row , colNo)	:
 		copy = []
-		for i in chess:
-			copy.append(i[:])
+		#i = 0
+		for i in xrange(colNo,self.dimension):
+			copy.append(row[i][:])
+			#i += 1
 		return copy
-	def putConstraint(self , chess , row , col):
-		y = row 
-		x = col
-		i = j = 0
-		#print row,col
-		# adding constraint to row nd column
-		while i < self.dimension:
-			chess[i][x] = 1		#col
-			chess[y][i] = 1		#row
-			i += 1
-		## adding constraint to left diagonal assume it is represented by line y = x + c since slope = 1 
-		#c = y - x
-		#xIntercept = -1 * c
-		#i = 0
-		#while i <= xIntercept :
-			#chess[xIntercept - i][i] = 1
-			#i += 1
-		## adding constraint to right diagonal assume it is represented by line y = -x + c since slope = -1 
-		#c = x + y
-		#yIntercept = c
-		#i = 0
-		#while i < self.dimension - yIntercept:
-			#chess[i][yIntercept + i] = 1
-			#i += 1
-		i = 0
-		while row + i < self.dimension and col + i < self.dimension:
-			chess[row + i][col + i] = 1
-			i += 1
-		i = 0
-		while row - i >= 0 and col + i < self.dimension:
-			chess[row - i][col + i] = 1	
-			i += 1
-		i = 0	
-		while row + i < self.dimension and col - i >= 0:
-			chess[row + i][col - i] = 1
-			i += 1
-		i = 0	
-		while row - i >= 0 and col + i < self.dimension:
-			chess[row - i][col + i] = 1
-			i += 1
-			
-		# dat plce where u hav put queen is overwritten by 1, it is guaranteed dat only one queen is overwritten,it is guaranteed else dis new queen 
-		# must not hav come in d first place
-		chess[row][col] = 2		#want 2 go 2 chess[y] row nd col no. x of dis row
+	
 		
-c = Chess(10)
-#print c
-col = 0
-limit = c.dimension % 2
+c = Chess(9)
+possibleAnswer = []
+print c
+for rowNo in c.row[0][:-2]:
+	copy = c.replicate(c.row,0)
+	c.choice(0,copy,rowNo,[])
+#col = 0
+#limit = c.dimension % 2
 
-while col < c.dimension:	#in d play function, for every greater row I m checking fr every column.hence I have made here sure dat 1st col also 
-	copy = c.replicate(c.board)
-	c.play(copy,0,col)	#keeps changing
-	col += 1
-#if c.dimension % 2 == 1:
+#while col < c.dimension:	#in d play function, for every greater row I m checking fr every column.hence I have made here sure dat 1st col also 
 	#copy = c.replicate(c.board)
-	#c.play(copy,0,col)
-#print c
-c.printSolutions()
-if c.solution == 0:
-	print "not possible on  a chessboard with dimension  %d" % c.dimension
-else:
-	print "No. of solutions = \t", c.solution
-print "No. of recursion",c.recursion
-
+	#c.play(copy,0,col)	#keeps changing
+	#col += 1
+##if c.dimension % 2 == 1:
+	##copy = c.replicate(c.board)
+	##c.play(copy,0,col)
+##print c
+#c.printSolutions()
+#if c.solution == 0:
+	#print "not possible on  a chessboard with dimension  %d" % c.dimension
+#else:
+	#print "No. of solutions = \t", c.solution
+#print "No. of recursion",c.recursion
+print "No. of solutions = \t", c.solution
